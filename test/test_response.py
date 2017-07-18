a112ac446184a88e80c5c78488f93c4f6370d1cd
薛丞宏 <ihcaoe@gmail.com>
2017-07-18 09:36:56 +0800

    加試驗

--- test/test_response.py
+++ test/test_response.py
@@ -38,5 +38,23 @@ class testResponse(TestCase):
         )
         self.assertContent(回應, b'tsiang5')
 
+    def test_failing(self):
+        request = self.factory.get(
+            '/path', HTTP_RANGE='bytes=17-20'
+        )
+        回應 = RangedFileResponse(
+            request, io.BytesIO(b'sui2khiau2tsiang5'), content_type='audio/wav'
+        )
+        self.assertEqual(回應.status_code, 416)
+
+    def test_overlapping(self):
+        request = self.factory.get(
+            '/path', HTTP_RANGE='bytes=10-20'
+        )
+        回應 = RangedFileResponse(
+            request, io.BytesIO(b'sui2khiau2tsiang5'), content_type='audio/wav'
+        )
+        self.assertContent(回應, 'khiau2')
+
     def assertContent(self, response, except_response):
         self.assertEqual(list(response.streaming_content)[0], except_response)
